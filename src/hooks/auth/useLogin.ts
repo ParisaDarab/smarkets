9921f8421854647c541c511acf544e61b24bcff5
unknown <parisadarab07@gmail.com>
2026-08-17 13:07:35 +0100

    login,home,events are added

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,5 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
-import { login } from '@/api/auth/authApi';
+import { login } from '@/api/auth/login';
 import type { LoginRequest } from '@/types/auth';
 
 export function useLogin() {
